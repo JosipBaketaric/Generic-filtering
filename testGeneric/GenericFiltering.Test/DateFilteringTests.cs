72785bdc239324273be36d456507799f9bbcce75
Josip Baketarić <josipba@in2.hr>
2018-03-21 09:28:21 +0100

    date tests added

--- testGeneric/GenericFiltering.Test/DateFilteringTests.cs
+++ testGeneric/GenericFiltering.Test/DateFilteringTests.cs
@@ -10,7 +10,7 @@ namespace GenericFiltering.Test
     {
 
         [TestMethod]
-        public void TestFilterDateResultCount()
+        public void TestFilterDateResultOneCount()
         {
             //arange
             var dateNow = DateTime.Now;
@@ -35,7 +35,151 @@ namespace GenericFiltering.Test
             //assert
             Assert.AreEqual(1, rez.Count);
         }
+        [TestMethod]
+        public void TestFilterDateResultTwoCount()
+        {
+            //arange
+            var dateNow = DateTime.Now;
 
-        
+            var domainList = new List<TestDomain>();
+            domainList.Add(new TestDomain { DATUM_TEST = dateNow });
+            domainList.Add(new TestDomain { DATUM_TEST = dateNow.AddDays(1) });
+            domainList.Add(new TestDomain { DATUM_TEST = dateNow.AddDays(2) });
+            domainList.Add(new TestDomain { DATUM_TEST = dateNow.AddDays(1) });
+            domainList.Add(new TestDomain { DATUM_TEST = dateNow.AddDays(2) });
+
+
+            var filter = new FilterDTO();
+            filter.DatumTest = dateNow.AddDays(1);
+
+            IQueryable<TestDomain> query = domainList.AsQueryable();
+
+            //act
+            query = FilterQuery.SetFilters<TestDomain, FilterDTO>(query, filter);
+            var rez = query.ToList();
+
+            //assert
+            Assert.AreEqual(2, rez.Count);
+        }
+
+        [TestMethod]
+        public void TestFilterDateResultGreaterCount()
+        {
+            //arange
+            var dateNow = DateTime.Now;
+
+            var domainList = new List<TestDomain>();
+            domainList.Add(new TestDomain { DATUM_TEST = dateNow });
+            domainList.Add(new TestDomain { DATUM_TEST = dateNow.AddDays(1) });
+            domainList.Add(new TestDomain { DATUM_TEST = dateNow.AddDays(2) });
+            domainList.Add(new TestDomain { DATUM_TEST = dateNow.AddDays(1) });
+            domainList.Add(new TestDomain { DATUM_TEST = dateNow.AddDays(2) });
+
+
+            var filter = new FilterDTO();
+            filter.DatumTest = dateNow;
+
+            var customList = new List<KeyValuePair<string, PropertyComparison>>();
+            customList.Add(new KeyValuePair<string, PropertyComparison>("DatumTest", PropertyComparison.Greater));
+
+            IQueryable<TestDomain> query = domainList.AsQueryable();
+
+            //act
+            query = FilterQuery.SetFilters<TestDomain, FilterDTO>(query, filter, null, customList);
+            var rez = query.ToList();
+
+            //assert
+            Assert.AreEqual(4, rez.Count);
+        }
+
+        [TestMethod]
+        public void TestFilterDateResultLessCount()
+        {
+            //arange
+            var dateNow = DateTime.Now;
+
+            var domainList = new List<TestDomain>();
+            domainList.Add(new TestDomain { DATUM_TEST = dateNow });
+            domainList.Add(new TestDomain { DATUM_TEST = dateNow.AddDays(1) });
+            domainList.Add(new TestDomain { DATUM_TEST = dateNow.AddDays(2) });
+            domainList.Add(new TestDomain { DATUM_TEST = dateNow.AddDays(1) });
+            domainList.Add(new TestDomain { DATUM_TEST = dateNow.AddDays(2) });
+
+
+            var filter = new FilterDTO();
+            filter.DatumTest = dateNow.AddDays(2);
+
+            var customList = new List<KeyValuePair<string, PropertyComparison>>();
+            customList.Add(new KeyValuePair<string, PropertyComparison>("DatumTest", PropertyComparison.Less));
+
+            IQueryable<TestDomain> query = domainList.AsQueryable();
+
+            //act
+            query = FilterQuery.SetFilters<TestDomain, FilterDTO>(query, filter, null, customList);
+            var rez = query.ToList();
+
+            //assert
+            Assert.AreEqual(3, rez.Count);
+        }
+
+        [TestMethod]
+        public void TestFilterDateResultGreaterOrEqualCount()
+        {
+            //arange
+            var dateNow = DateTime.Now;
+
+            var domainList = new List<TestDomain>();
+            domainList.Add(new TestDomain { DATUM_TEST = dateNow });
+            domainList.Add(new TestDomain { DATUM_TEST = dateNow.AddDays(1) });
+            domainList.Add(new TestDomain { DATUM_TEST = dateNow.AddDays(2) });
+            domainList.Add(new TestDomain { DATUM_TEST = dateNow.AddDays(1) });
+            domainList.Add(new TestDomain { DATUM_TEST = dateNow.AddDays(2) });
+
+
+            var filter = new FilterDTO();
+            filter.DatumTest = dateNow;
+
+            var customList = new List<KeyValuePair<string, PropertyComparison>>();
+            customList.Add(new KeyValuePair<string, PropertyComparison>("DatumTest", PropertyComparison.GreaterOrEqual));
+
+            IQueryable<TestDomain> query = domainList.AsQueryable();
+
+            //act
+            query = FilterQuery.SetFilters<TestDomain, FilterDTO>(query, filter, null, customList);
+            var rez = query.ToList();
+
+            //assert
+            Assert.AreEqual(5, rez.Count);
+        }
+
+        [TestMethod]
+        public void TestFilterDateResultLessOrEqualCount()
+        {
+            //arange
+            var dateNow = DateTime.Now;
+
+            var domainList = new List<TestDomain>();
+            domainList.Add(new TestDomain { DATUM_TEST = dateNow });
+            domainList.Add(new TestDomain { DATUM_TEST = dateNow.AddDays(1) });
+            domainList.Add(new TestDomain { DATUM_TEST = dateNow.AddDays(2) });
+            domainList.Add(new TestDomain { DATUM_TEST = dateNow.AddDays(1) });
+            domainList.Add(new TestDomain { DATUM_TEST = dateNow.AddDays(2) });
+
+
+            var filter = new FilterDTO();
+            filter.DatumTest = dateNow;
+
+            var customList = new List<KeyValuePair<string, PropertyComparison>>();
+            customList.Add(new KeyValuePair<string, PropertyComparison>("DatumTest", PropertyComparison.LessOrEqual));
+
+            IQueryable<TestDomain> query = domainList.AsQueryable();
+
+            //act
+            query = FilterQuery.SetFilters<TestDomain, FilterDTO>(query, filter, null, customList);
+            var rez = query.ToList();
+
+            //assert
+            Assert.AreEqual(1, rez.Count);
+        }
     }
 }
